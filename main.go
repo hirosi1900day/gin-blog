7718d5a00dc7e0c917d4d5245d9a640227adbf38
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2024-03-23 14:48:21 +0900

    item intter faceを定義

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"gin-fleamarket/models"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	items := []models.Item{
+		{ID: 1, Name: "item1", Price: 1000, Description: "This is item1", SoldOut: false},
+		{ID: 2, Name: "item2", Price: 2000, Description: "This is item2", SoldOut: true},
+		{ID: 3, Name: "item3", Price: 3000, Description: "This is item3", SoldOut: false},
+	}
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
