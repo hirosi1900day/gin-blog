70760ad396f8f46ae2013d956587f0ce3ba388e4
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2024-03-23 15:04:32 +0900

    側のintergace作成

--- main.go
+++ main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"gin-fleamarket/models"
-
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	items := []models.Item{
-		{ID: 1, Name: "item1", Price: 1000, Description: "This is item1", SoldOut: false},
-		{ID: 2, Name: "item2", Price: 2000, Description: "This is item2", SoldOut: true},
-		{ID: 3, Name: "item3", Price: 3000, Description: "This is item3", SoldOut: false},
-	}
+	// items := []models.Item{
+	// 	{ID: 1, Name: "item1", Price: 1000, Description: "This is item1", SoldOut: false},
+	// 	{ID: 2, Name: "item2", Price: 2000, Description: "This is item2", SoldOut: true},
+	// 	{ID: 3, Name: "item3", Price: 3000, Description: "This is item3", SoldOut: false},
+	// }
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
