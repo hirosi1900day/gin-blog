cc4494706f7bc629a947bf29b2094b39543c7e23
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2024-03-23 16:27:14 +0900

    updateロジック作成

--- controllers/item_controller.go
+++ controllers/item_controller.go
@@ -14,6 +14,7 @@ type IItemController interface {
 	FindAll(ctx *gin.Context)
 	FindById(ctx *gin.Context)
 	Create(ctx *gin.Context)
+	Update(ctx *gin.Context)
 }
 
 type ItemController struct {
@@ -67,6 +68,35 @@ func (c *ItemController) Create(ctx *gin.Context) {
 	item, err := c.service.Create(input)
 
 	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Unexpected error"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"data": item})
+}
+
+func (c *ItemController) Update(ctx *gin.Context) {
+	itemId, err := strconv.ParseUint(ctx.Param("id"), 10, 64)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid id"})
+		return
+	}
+
+	var input dto.UpdateItemInput
+
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	item, err := c.service.Update(uint(itemId), input)
+
+	if err != nil {
+		if err.Error() == "Item not found" {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Unexpected error"})
 		return
 	}
